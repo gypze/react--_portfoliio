40fef2779d2cae9c1642085b7ff58bf7bcef7e68
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-30 15:06:21 -0600

    setting up

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+impoort Navigation from " ./Navigation";
+
+function Header() {
+    return (
+        <header>
+            <h1>Your Name</h1>
+            <Navigation />
+        </header>
+    )
+}
+
+export default Header;
