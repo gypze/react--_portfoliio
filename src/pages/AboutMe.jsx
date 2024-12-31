9b8cb416aab28e1883b5bd2059f40a2a9ddca610
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-31 00:19:09 -0600

    fixing code

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,8 +4,8 @@ function AboutMe() {
     return (
         <section id="about">
             <h2>About Me</h2>
-            <img src="/path-to-your-phot.jpg" alt="Your Name" />
-            <p>Write a short bio about yourself here!</p>
+            <img src="c:\Users\Gypze\OneDrive\Pictures\Camera Roll\E5D89AC6- (1).JPG" alt="Gypze" />
+            <p>Hi, I’m Gypze, a dedicated and enthusiastic junior developer currently building a solid foundation in web development technologies. My journey into coding is fueled by a passion for continuous learning and creating impactful projects. I thrive on challenges and enjoy exploring modern frameworks like React and technologies like PostgreSQL. When I'm not coding, you’ll find me gaming, making jewelry, or hanging out with my cats. Let’s connect and build something amazing together!</p>
         </section>
     );
 }
