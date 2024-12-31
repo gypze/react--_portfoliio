ffdab80366b3d071deaf8a36feb28eada83ca45b
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-12-31 00:11:44 -0600

    added info

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,24 +7,24 @@ function Portfolio() {
             <h2>Portfolio</h2>
             <Project
                 title="Project 1"
-                image="/path-to-your-image.jpg"
-                deployedLink="https://www.example.com"
-                repoLink="https://www.github.com"
+                image="c:\Users\Gypze\OneDrive\Pictures\Screenshots\Screenshot 2024-12-31 000424.png"
+                deployedLink="https://gypzes-tech-blog.onrender.com"
+                repoLink="https://github.com/gypze/Gypzes-Tech-Blog.git"
             />
             <Project
                 title="Project 2"
-                image="/path-to-your-image.jpg"
-                deployedLink="https://www.example.com"
-                repoLink="https://www.github.com"
+                image=""C:\Users\Gypze\OneDrive\Pictures\Screenshots\Screenshot 2024-12-31 001013.png""
+                deployedLink="https://gypzes-pwa.onrender.com"
+                repoLink="https://github.com/gypze/Gypzes-PWA.git"
             />
             <Project
                 title="Project 3"
-                image="/path-to-your-image.jpg"
-                deployedLink="https://www.example.com"
-                repoLink="https://www.github.com"
+                image=""C:\Users\Gypze\OneDrive\Pictures\Screenshots\Screenshot 2024-12-31 000743.png""
+                deployedLink="https://gypze.github.io/Gypze-s-Amazing-Blog/"
+                repoLink="https://github.com/gypze/Gypze-s-Amazing-Blog.git"
             />
         </section>
     );
 }
     
-export default
+export default Portfolio;
